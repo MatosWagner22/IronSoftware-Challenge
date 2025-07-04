a74535797ecc7f7838ba486cac8f4b7ffafaadfb
MatosWagner22 <matoswagner22@gmail.com>
2025-07-04 13:10:32 -0400

    Agregar archivos de proyecto.

---
+++ PhoneKeypad/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PhoneKeypadLib;
+
+Console.WriteLine("Old Phone Keypad Simulator");
+Console.WriteLine("Enter key sequence (end with '#'):");
+
+string input = Console.ReadLine();
+string output = PhoneKeypad.Convert(input);
+
+Console.WriteLine($"Output: {output}");
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
---
+++ PhoneKeypadLib/PhoneKeypad.cs
@@ -0,0 +1,22 @@
+﻿namespace PhoneKeypadLib
+{
+    public static class PhoneKeypad
+    {
+        static readonly string[] KEYS = { 
+            "&'(",
+            "ABC",
+            "DEF",
+            "GHI",
+            "JKL",
+            "MNO",
+            "PQRS",
+            "TUV",
+            "WXYZ",
+            "   "
+        };
+        public static string Convert(string input)
+        {
+            return null;
+        }
+    }
+}
